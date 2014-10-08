05852e34fe2288052407cd8d7d8ef5e2c5b27756
jsn筆電 <jsn@wondershow.tw>
2014-10-08 15:09:07 +0800

    v1.0

---
+++ application/views/layout/contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>contact</title>
+</head>
+<body>
+	
+	<div>
+		聯絡頁面	
+	</div>
+</body>
+</html>
---
+++ application/views/layout/news.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>新聞</title>
+</head>
+<body>
+	<? $a=0; while($a++<10) { ?>
+		<div>這是新聞列表</div>
+	<? } ?>
+</body>
+</html>
